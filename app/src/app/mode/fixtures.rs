1bcef8cea3077477dff4d43ab80e921d26253b5a
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-01-31 22:01:11 +0100

    Factor out the add Fixture functionality

--- app/src/app/mode/fixtures.rs
+++ app/src/app/mode/fixtures.rs
@@ -30,6 +30,7 @@ impl<'a> App<'a>{
                                 if ui.button("Remove").clicked() {
                                     remove_list.push((universe, dev_id));
                                 }
+                                //todo: add edit button
                                 ui.end_row();
                             }
                         });
@@ -44,85 +45,89 @@ impl<'a> App<'a>{
                 }
             }
             if ui.button("Add Fixture").clicked() {
-                let mut name = "";
-                let mut universe = 1;
-                let mut start_id = 0;
-                let mut universe_err = None;
-                let mut device_insert_err = None;
-                let mut opt_fixture = (Vec::<Arc<str>>::new(), None);
-                let grid_id = get_id();
-                self.popups.push_back(popup_creator("Add Fixture", move |app, ui|{
-                    egui::Grid::new(grid_id)
-                        .show(ui, |ui|{
-                            let ui_universe;
-                            let ui_channel;
-                            ui.label("Name: ");
-                            ui.text_edit_singleline(&mut name);
-                            ui.end_row();
+                self.open_add_fixture_ui(ui)
+            }
+        });
+    }
 
-                            ui.label("Universe: ");
-                            ui_universe = egui::DragValue::new(&mut universe)
-                                .clamp_range(0..=MAX_UNIVERSE_ID-1)
-                                .ui(ui);
+    fn open_add_fixture_ui(&mut self, ui:&mut egui::Ui) {
+        let mut name = "";
+        let mut universe = 1;
+        let mut start_id = 0;
+        let mut universe_err = None;
+        let mut device_insert_err = None;
+        let mut opt_fixture = (Vec::<Arc<str>>::new(), None);
+        let grid_id = get_id();
+        self.popups.push_back(popup_creator("Add Fixture", move |app, ui|{
+            egui::Grid::new(grid_id)
+                .show(ui, |ui|{
+                    let ui_universe;
+                    let ui_channel;
+                    ui.label("Name: ");
+                    ui.text_edit_singleline(&mut name);
+                    ui.end_row();
 
-                            ui.end_row();
+                    ui.label("Universe: ");
+                    ui_universe = egui::DragValue::new(&mut universe)
+                        .clamp_range(0..=MAX_UNIVERSE_ID-1)
+                        .ui(ui);
 
-                            ui.label("Start Channel: ");
-                            ui_channel = egui::DragValue::new(&mut start_id)
-                                .clamp_range(0..=MAX_CHANNEL_ID-1)
-                                .ui(ui);
-                            ui.end_row();
+                    ui.end_row();
 
-                            ui.menu_button(format!("{} Fixture", if opt_fixture.0.is_empty() {"Set"} else {"Change"}), |ui|{
-                                FIXTURE_STORE.build_menu(ui, &mut opt_fixture);
-                            });
-                            let vec_unset = opt_fixture.0.is_empty();
-                            if vec_unset {
-                                ui.label("unset");
-                            }else{
-                                let mut path = String::new();
-                                for i in &opt_fixture.0{
-                                    path.push('/');
-                                    path.push_str(i.as_ref());
-                                }
-                                ui.label(path);
-                            }
-                            ui.end_row();
-                            ui.horizontal(|ui|{
-                                if let Some(fixture) = &opt_fixture.1 {
-                                    if ui.button("Add").clicked() {
-                                        match app.data.create_or_get_universe(universe){
+                    ui.label("Start Channel: ");
+                    ui_channel = egui::DragValue::new(&mut start_id)
+                        .clamp_range(0..=MAX_CHANNEL_ID-1)
+                        .ui(ui);
+                    ui.end_row();
+
+                    ui.menu_button(format!("{} Fixture", if opt_fixture.0.is_empty() {"Set"} else {"Change"}), |ui|{
+                        FIXTURE_STORE.build_menu(ui, &mut opt_fixture);
+                    });
+                    let vec_unset = opt_fixture.0.is_empty();
+                    if vec_unset {
+                        ui.label("unset");
+                    }else{
+                        let mut path = String::new();
+                        for i in &opt_fixture.0{
+                            path.push('/');
+                            path.push_str(i.as_ref());
+                        }
+                        ui.label(path);
+                    }
+                    ui.end_row();
+                    ui.horizontal(|ui|{
+                        if let Some(fixture) = &opt_fixture.1 {
+                            if ui.button("Add").clicked() {
+                                match app.data.create_or_get_universe(universe){
+                                    Err(err) => {
+                                        universe_err = Some(err);
+                                    }
+                                    Ok(universe)=>{
+                                        match universe.try_insert(Device::new(Arc::from(name), start_id, fixture.clone())){
+                                            Ok(_) => {}
                                             Err(err) => {
-                                                universe_err = Some(err);
-                                            }
-                                            Ok(universe)=>{
-                                                match universe.try_insert(Device::new(Arc::from(name), start_id, fixture.clone())){
-                                                    Ok(_) => {}
-                                                    Err(err) => {
-                                                        device_insert_err = Some(err);
-                                                    }
-                                                }
+                                                device_insert_err = Some(err);
                                             }
                                         }
                                     }
                                 }
+                            }
+                        }
 
-                                if let Some(err) = universe_err{
-                                    ui_universe.ctx.debug_painter().error(ui_universe.rect.left_bottom(), err.to_string());
-                                }
-                                if let Some(err) = device_insert_err{
-                                    ui_channel.ctx.debug_painter().error(ui_channel.rect.left_bottom(), err.to_string());
-                                }
+                        if let Some(err) = universe_err{
+                            ui_universe.ctx.debug_painter().error(ui_universe.rect.left_bottom(), err.to_string());
+                        }
+                        if let Some(err) = device_insert_err{
+                            ui_channel.ctx.debug_painter().error(ui_channel.rect.left_bottom(), err.to_string());
+                        }
 
-                                if ui.button("Clear").clicked() {
-                                    universe = 1;
-                                    start_id = 0;
-                                    opt_fixture = (Vec::new(), None);
-                                }
-                            });
-                        });
-                }));
-            }
-        });
+                        if ui.button("Clear").clicked() {
+                            universe = 1;
+                            start_id = 0;
+                            opt_fixture = (Vec::new(), None);
+                        }
+                    });
+                });
+        }));
     }
 }
